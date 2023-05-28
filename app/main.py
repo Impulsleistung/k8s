8eb7abe95dab445cfea0498f4c3cae8460770b92
impulsleistung <info@impulsleistung.de>
2023-05-28 23:37:01 +0200

    update

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/random-numbers")
+def random_numbers(n: int = 10):
+    """Generate a dataframe with `n` random numbers."""
+    df = pd.DataFrame(pd.np.random.randint(0, 100, size=(n, 2)))
+    return df
